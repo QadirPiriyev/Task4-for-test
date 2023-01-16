71d6bd912ccd44197e660313fe816a2264464449
Qadir <Qadir@DESKTOP-9F9K47Q>
2023-01-16 11:48:25 -0800

    Add project files.

---
+++ Task4!/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Drawing;
+
+namespace Task4_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("           programa xosh gelmisiniz");
+            Console.Write("Nece kitab yaratmaq isdeyirsiz :");
+            int a = helper.yoxlama();
+            book[] kitab = new book[a];
+            for (int i = 0; i < kitab.Length; i++)
+            {
+                book s = new book();
+                Console.Write($"{i + 1})Muellifin adini daxil edin :");
+                s.muellif = helper.stringChecker();
+                Console.Write($"{i + 1})Eserin adini daxil edin :");
+                s.eser = helper.stringChecker();
+                Console.Write($"{i + 1})Kitab nece sehiveden ibaretdir? :");
+                label2:
+                s.sehife = helper.yoxlama2();
+                if (s.sehife<10)
+                {
+                    Console.WriteLine("duzgun sehife daxil edin");
+                    goto label2;
+                }
+                Console.Write($"{i + 1})Kitabin nesr ilini daxil edin :");
+                s.nesr = helper.yoxlama3();
+                kitab[i] = s;
+            }
+            Console.WriteLine("--------------------");
+            foreach (book item in kitab)
+            {
+                Console.WriteLine(item);
+            }
+
+            label1:
+            Console.WriteLine("Nusxe yaratmaq isdeyirsizse 1-e basin eks halda 0-a basin");
+            int test= helper.yoxlama4();
+            int copyer = helper.ArrayChecker(a, test);
+            if (copyer == -1){ goto label1; }
+            
+            
+            Console.WriteLine($"copyasi yaradilacaq kitap = {kitab[copyer-1]}");
+            
+        }   
+    }    
+	
+}
---
+++ Task4!/helper.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+
+namespace Task4_
+{
+    internal class helper
+    {
+       internal static int yoxlama()
+        {
+        Label1:
+            int a;
+
+            string inputvalue = Console.ReadLine();
+            int.TryParse(inputvalue, out a);
+            if (int.TryParse(inputvalue, out a) != true)
+            {
+                Console.WriteLine("Tam ededi daxil edin");
+                goto Label1;
+            }
+            else
+            {
+                return a;
+            }
+        }
+       internal static int yoxlama2()
+        {
+            int a;
+        Label1:
+            string inputvalue = Console.ReadLine();
+            int.TryParse(inputvalue, out a);
+            if (int.TryParse(inputvalue, out a) != true)
+            {
+                Console.WriteLine("Duzgun sehive daxil edin");
+                goto Label1;
+            }
+            else
+            {
+                return a;
+            }
+        }//sehive ucun
+       internal static int yoxlama3()
+        {
+            int a;
+        Label1:
+            string inputvalue = Console.ReadLine();
+            int.TryParse(inputvalue, out a);
+            if (int.TryParse(inputvalue, out a) != true)
+            {
+                Console.WriteLine("Duzgun nesr ili daxil edin");
+                goto Label1;
+            }
+            else
+            {
+                return a;
+            }
+        }//nesr ucun
+       internal static int yoxlama4()//nusxe ucun
+        {
+        label1:
+            int b = yoxlama();
+            if (b == 1)
+            {
+                Console.WriteLine("Hansi eserin nusxesini isdeyirsiz? ");
+                int value = yoxlama2();
+                return value;
+
+            }
+            else if (b == 0)
+            {
+                Console.WriteLine("Umid edirik size komek ola bildik");
+            }
+            else
+            {
+                Console.WriteLine("Yanlis!");
+                goto label1;
+            }
+            return b;
+
+        }
+        internal static string stringChecker()
+        {
+            l1:
+            string input=Console.ReadLine();
+            if (int.TryParse(input,out int a))
+            {
+                Console.WriteLine("Duzgun daxil edin");
+                goto l1;
+            }
+            return input;
+        }
+        internal static int ArrayChecker(int limit,int Copy)
+        {
+
+            if ( limit < Copy)
+            {
+                Console.WriteLine("daxil ettdiyiniz melumat tapilmadi");
+                return -1;
+            }
+            else
+            {
+                return Copy;
+            }
+       
+        }
+    }
+}
